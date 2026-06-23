92cb47f48ea1f7ed79f3ee8457b683f0362f2f19
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2026-06-23 09:46:51 +0000

    numberandMath

---
+++ 01_basics/numbersAndMaths.js
@@ -0,0 +1,40 @@
+let score =400;
+//console.log(typeof score);
+
+const balance = new Number(100);
+//console.log(typeof balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(4));
+
+const ortherNumber= 123.896;
+//console.log(ortherNumber.toPrecision(4));
+
+
+const hundreads=1000000;
+//console.log(hundreads.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++++++++MATHS++++++++++++++++++++++
+
+
+
+
+// console.log(Math.abs(-191));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.min(2,4,1,5));
+// console.log(Math.max(2,4,1,5));
+
+
+let min = 10;
+let max = 20;
+
+console.log(Math.floor((Math.random()* (max-min))+1)+min);
+
+
+
+
