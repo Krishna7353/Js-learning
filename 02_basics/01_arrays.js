a3a41031921e48d6fefda11303240ebd7937693a
Krishna7353 <167784358+Krishna7353@users.noreply.github.com>
2026-06-23 12:52:10 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,23 @@
+//Arrays
+
+const myArr=[0,1,2,3,4,5,6];
+const myHeros=["shaktiman","naagraj"]; 
+const myArr2= new Array(1,2,33,4);
+// const myArr2=[1,2,3,4];
+// myArr.push(5);
+// myArr.pop();
+
+
+myArr.unshift(6);
+myArr.shift();
+
+console.log("A ",myArr);
+console.log(myArr.slice(1,3))
+
+console.log("B ",myArr);
+console.log(myArr.splice(1,3));
+
+console.log("c ",myArr);
+
+
+
